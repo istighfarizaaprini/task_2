ec0ddd79a25ec9398b10c196eab3ee7f340e5f6c
Istighfariza Aprini <istighfariza.119140059@student.itera.ac.id>
2021-10-05 19:41:29 +0700

    Update Istighfariza_Detail_infoPendaftaran.php

--- Istighfariza_Detail_infoPendaftaran.php
+++ Istighfariza_Detail_infoPendaftaran.php
@@ -8,11 +8,25 @@
     <tr>
         <td>Nama</td>
         <td>Tempat Lahir</td>
+        <td>Tanggal Lahir</td>
+        <td>Jenis Kelamin</td>
+        <td>Alamat</td>
+        <td>Riwayat Penyakit</td>
+        <td>E-mail</td>
+        <td>No HP</td>
+        <td>Agama</td>
     </tr>
 
     <tr>
         <td><?php echo $_GET ["nama"];?></td>
+        <td><?php echo $_GET ["tempat_lahir"];?></td>
         <td><?php echo $_GET ["tgl_lahir"];?></td>
+        <td><?php echo $_GET ["sex"];?></td>
+        <td><?php echo $_GET ["alamat"];?></td>
+        <td><?php echo $_GET ["disease"];?></td>
+        <td><?php echo $_GET ["email"];?></td>
+        <td><?php echo $_GET ["no_telp"];?></td>
+        <td><?php echo $_GET ["agama"];?></td>
     </tr>
 </body>
 <html>
