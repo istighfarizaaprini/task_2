90e433f142126d7519aa44fc3a3e4a2038042e28
Istighfariza Aprini <istighfariza.119140059@student.itera.ac.id>
2021-10-05 20:30:21 +0700

    Update Istighfariza_Detail_infoPendaftaran.php

--- Istighfariza_Detail_infoPendaftaran.php
+++ Istighfariza_Detail_infoPendaftaran.php
@@ -3,30 +3,35 @@
     <title>Detail Informasi Pendaftaran</title>
 </head>
 <body>
-    <table border="3">
+    <h1>
+        Berikut adalah detail Informasi Pendaftaran
+    </h1>
 
-    <tr>
-        <td>Nama</td>
-        <td>Tempat Lahir</td>
-        <td>Tanggal Lahir</td>
-        <td>Jenis Kelamin</td>
-        <td>Alamat</td>
-        <td>Riwayat Penyakit</td>
-        <td>E-mail</td>
-        <td>No HP</td>
-        <td>Agama</td>
-    </tr>
+    <center><table border="3">
+        <tr>
+            <td>Nama</td>
+            <td>Tempat Lahir</td>
+            <td>Tanggal Lahir</td>
+            <td>Jenis Kelamin</td>
+            <td>Alamat</td>
+            <td>Riwayat Penyakit</td>
+            <td>E-mail</td>
+            <td>No HP</td>
+            <td>Agama</td>
+        </tr>
 
-    <tr>
-        <td><?php echo $_GET ["nama"];?></td>
-        <td><?php echo $_GET ["tempat_lahir"];?></td>
-        <td><?php echo $_GET ["tgl_lahir"];?></td>
-        <td><?php echo $_GET ["sex"];?></td>
-        <td><?php echo $_GET ["alamat"];?></td>
-        <td><?php echo $_GET ["disease"];?></td>
-        <td><?php echo $_GET ["email"];?></td>
-        <td><?php echo $_GET ["no_telp"];?></td>
-        <td><?php echo $_GET ["agama"];?></td>
-    </tr>
+        <tr>
+            <td><?php echo $_GET ["nama"];?></td>
+            <td><?php echo $_GET ["tempat_lahir"];?></td>
+            <td><?php echo $_GET ["tgl_lahir"];?></td>
+            <td><?php echo $_GET ["sex"];?></td>
+            <td><?php echo $_GET ["alamat"];?></td>
+            <td><?php echo $_GET ["disease"];?></td>
+            <td><?php echo $_GET ["email"];?></td>
+            <td><?php echo $_GET ["no_telp"];?></td>
+            <td><?php echo $_GET ["agama"];?></td>
+        </tr>
+    </table></center>
+
 </body>
 <html>
