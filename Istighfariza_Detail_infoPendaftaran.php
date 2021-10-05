93beed379b3551f16d4a7081480b3c054b5cd7d0
Istighfariza Aprini <istighfariza.119140059@student.itera.ac.id>
2021-10-05 19:29:56 +0700

    Create Istighfariza_Detail_infoPendaftaran.php

---
+++ Istighfariza_Detail_infoPendaftaran.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <title>Detail Informasi Pendaftaran</title>
+</head>
+<body>
+    <table border="0" style="center"; color:"black">
+
+    <tr>
+        <td>Nama</td>
+        <td>Tempat Lahir</td>
+    </tr>
+
+    <tr>
+        <td><?php echo $_GET ["nama"];?></td>
+        <td><?php echo $_GET ["tgl_lahir"];?></td>
+    </tr>
+</body>
+<html>
